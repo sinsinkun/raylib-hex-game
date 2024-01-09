05e4438f475a7eb3b3f729c8111ac025aa9e2342
Chang Xiao <chang.x1994@gmail.com>
2024-01-09 00:37:05 -0500

    Basic drawn shader

---
+++ assets/test.frag
@@ -0,0 +1,8 @@
+uniform float t;
+uniform vec2 screen_size;
+
+void main() {
+  vec2 coord = gl_FragCoord.xy / screen_size;
+
+  gl_FragColor = vec4(coord * 0.5, 0.1, 1.0);
+}
---
+++ main.cpp
@@ -0,0 +1,58 @@
+#include <cstdio> // imports printf
+#include "raylib.h"
+
+#define GLSL_VERSION 330
+
+// main event loop
+void EventLoop(Shader shader) {
+  ClearBackground(BLACK);
+  if (IsWindowFocused()) {
+    // get world parameters
+    int w = GetScreenWidth();
+    int h = GetScreenHeight();
+    int fps = GetFPS();
+    double elapsed = GetTime();
+    // Vector2 mousePos = GetMousePosition();
+
+    // add uniforms to shader
+    float screenSize[2] = { (float)w, (float)h };
+    SetShaderValue(shader, GetShaderLocation(shader, "t"), &elapsed, SHADER_UNIFORM_FLOAT);
+    SetShaderValue(shader, GetShaderLocation(shader, "screen_size"), &screenSize, SHADER_UNIFORM_VEC2);
+    // draw background
+    BeginShaderMode(shader);
+      DrawRectangle(0, 0, w, h, WHITE);
+    EndShaderMode();
+
+    // printf("Debug - %i %f\n", fps, elapsed);
+    DrawText(TextFormat("%i %f", fps, elapsed), 10, 10, 20, WHITE);
+    DrawPoly((Vector2){ (float)w/2, (float)h/2 }, 4, 80, elapsed * 10, WHITE);
+
+  } else {
+    DrawText("Pay attention to me", 10, 10, 24, RED);
+  }
+}
+
+int main() {
+  // --- INITIALIZATION ---
+  // SetWindowIcon(image);
+  SetConfigFlags(FLAG_WINDOW_RESIZABLE); // Window configuration flags
+  InitWindow(800, 600, "Test Window");
+  SetWindowMinSize(400, 300);
+  SetTargetFPS(120);
+
+  Shader shader = LoadShader(0, TextFormat("assets/test.frag", GLSL_VERSION));
+  
+  // --- EVENT LOOP ---
+  printf("\n\n\n-- Starting Event Loop --\n");
+  while (!WindowShouldClose()) {
+    BeginDrawing();
+      EventLoop(shader);
+    EndDrawing();
+  }
+
+  // --- CLEAN UP ---
+  UnloadShader(shader);
+  CloseWindow();
+
+  return 0;
+}
